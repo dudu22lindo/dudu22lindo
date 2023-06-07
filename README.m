cf5e43e6210dc2aa67ae2a98de475841a235526e
dudu22lindo <135868281+dudu22lindo@users.noreply.github.com>
2023-06-07 15:05:43 -0300

    carlos / README.m
    sou carlos é jogo bola é faço sites kk

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Oi, eu sou @carloseduardo
+- 👀 Estou interessado
+- 🌱 Atualmente estou programando
+- 💞️ Estou pr
+- 📫 Como chegar até mim...
+
+<!---
+dudu22lindo/dudu22lindo is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
