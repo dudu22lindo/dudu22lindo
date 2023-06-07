cac8b90b206cbc7f91f00703a87fefa50a572035
dudu22lindo <135868281+dudu22lindo@users.noreply.github.com>
2023-06-07 15:15:13 -0300

    README.m

--- README.m
+++ README.m
@@ -1,8 +1,10 @@
-- 👋 Oi, eu sou @carloseduardo
-- 👀 Estou interessado
-- 🌱 Atualmente estou programando
-- 💞️ Estou pr
-- 📫 Como chegar até mim...
+- 👋 Oi, eu sou carloseduardo
+- 👀 deselvolvedor Front-End
+- 🌱 Atualmente jogando futebol
+- 💞️ casado com @acllarays_
+- 📫 Como chegar até mim 21 977207762
+instragam: @cadunpj
+
 
 <!---
 dudu22lindo/dudu22lindo is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
